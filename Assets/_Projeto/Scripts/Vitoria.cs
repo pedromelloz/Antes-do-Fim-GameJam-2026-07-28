18ac7fd4d3e7c785e3afd9c953e5fe100a6a3818
pedromelloz <pedromiguelkm@gmail.com>
2026-07-29 06:52:18 -0300

    Botões da cena game over

--- Assets/_Projeto/Scripts/Vitoria.cs
+++ Assets/_Projeto/Scripts/Vitoria.cs
@@ -3,15 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Vitoria : MonoBehaviour
 {
-    [Header("Configuração de Cena")]
-    public string nomeCenaCinematic = "CenaMeteoro"; // Nome da cena que vamos criar
+    public string nomeCenaVitoria = "Vitoria"; // Nome da cena que acabamos de criar
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Vai para a cena da animação/meteoro caindo
-            SceneManager.LoadScene(nomeCenaCinematic);
+            SceneManager.LoadScene(nomeCenaVitoria);
         }
     }
 }
